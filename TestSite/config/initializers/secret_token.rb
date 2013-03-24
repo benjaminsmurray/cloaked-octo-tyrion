e5a04543ddae9b043a2231c84fc68feab329140f
benjaminsmurray <benjaminsmurray@gmail.com>
2013-03-24 23:03:34 +0000

    add new app

---
+++ TestSite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSite::Application.config.secret_token = '9fa9d473e917ad4370f911afe8bce1a4465a7ad8d258ffb6fb38fb0b29449aa629e82457a4e3f6f005ad3d3849c2767a13a5dbe6af31dfc5748d01c4efede671'
