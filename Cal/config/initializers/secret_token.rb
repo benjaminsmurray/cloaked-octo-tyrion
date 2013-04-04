fe891ee39a53ce44e19257043b7b5dfd80d6a640
benjaminsmurray <benjaminsmurray@gmail.com>
2013-04-04 02:04:44 +0000

    initial

---
+++ Cal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cal::Application.config.secret_token = '4556aa6bbb718bd83cc7bb5e9d9bd63ca5902162795f35cab150fe075372156ff87242fa5ed89c9e0dbd13537a7469b019820ca7dad9422b0c12a05d4143ed16'
