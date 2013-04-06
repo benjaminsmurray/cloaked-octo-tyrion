fad07c0b42fce777bad4975d9ffa16ea7d96a5fb
benjaminsmurray <benjaminsmurray@gmail.com>
2013-04-06 19:09:05 +0000

    remove old event

--- Cal/app/models/myeventshandler.rb
+++
@@ -1,3 +0,0 @@
-class Myeventshandler < ActiveRecord::Base
-   attr_accessible :username, :eventid
-end
--- Cal/db/migrate/20130406083215_create_myeventshandlers.rb
+++
@@ -1,9 +0,0 @@
-class CreateMyeventshandlers < ActiveRecord::Migration
-  def change
-    create_table :myeventshandlers do |t|
-      t.string :username
-      t.string :eventid
-      t.timestamps
-    end
-  end
-end
